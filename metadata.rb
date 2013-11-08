dabab2bcfe33e55e826cf073909ebf1449239718
Matt Stratton <matt.stratton@gmail.com>
2013-11-08 14:03:14 -0600

    Add dependencies to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,5 @@ description      'Installs/Configures xm-demo-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends "windows", "~> 1.11.0"
+depends "iis", "~> 1.5.4"
