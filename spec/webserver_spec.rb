450a18bfc648bebc25c1cd7f9db54fa191fb9f4b
Matt Stratton <matt.stratton@gmail.com>
2013-11-12 13:13:36 -0600

    Add webserver spec test

--- spec/webserver_spec.rb
+++ spec/webserver_spec.rb
@@ -2,8 +2,15 @@ require 'chef/config'
 require 'spec_helper'
 
 describe 'xm-demo-cookbook::webserver' do
-  
-  
-  
-  
+  let (:chef_run) do
+    runner = ChefSpec::ChefRunner.new(
+      'platform' => 'windows',
+      'version' => '2008R2'
+      )
+    runner.converge('xm-demo-cookbook::webserver')
+end
+
+it 'should start a service called W3SVC' do
+  expect(chef_run).to start_service('W3SVC')
+  expect(chef_run).to set_service_to_start_on_boot('W3SVC')
 end
