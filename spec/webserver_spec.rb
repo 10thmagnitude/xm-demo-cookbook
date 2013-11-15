0d9615b603e30801a1e1fe7bfe8af56e54444bcf
Matt Stratton <matt.stratton@gmail.com>
2013-11-15 14:12:47 -0600

    Attempt to remove cookbook dependency

--- spec/webserver_spec.rb
+++ spec/webserver_spec.rb
@@ -1,8 +1,6 @@
 require 'chef/config'
 require 'spec_helper'
 
-expect(chef_run).to include_recipe('windows::default')
-
 describe 'xm-demo-cookbook::webserver' do
   let (:chef_run) do
     runner = ChefSpec::Runner.new(
@@ -13,6 +11,7 @@ describe 'xm-demo-cookbook::webserver' do
   end
 
   it 'should install the IIS role' do
+    #expect(chef_run).to include_recipe('windows::default')
     expect(chef_run).to install_feature('IIS-WebServerRole')
   end
 end
